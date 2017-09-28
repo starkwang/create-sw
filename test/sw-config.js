e137941f1c74ef6e7065cc9f54acf7010ba828d8
Weijia Wang <381152119@qq.com>
2017-09-28 09:58:58 +0800

    add sw-toolbox

--- test/sw-config.js
+++ test/sw-config.js
@@ -1,12 +1,16 @@
 module.exports = {
-    staticFileGlobs: [
-      'dist/**'
-    ],
-    stripPrefixMulti: {
-      'dist/': '/'
-    },
-    swFile: '/dist/service-worker.js',
-    externals: [
-        'https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js'
-    ]
-  };
+  staticFileGlobs: [
+    'dist/**'
+  ],
+  stripPrefixMulti: {
+    'dist/': '/'
+  },
+  swFile: '/dist/service-worker.js',
+  externals: [
+    'https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js'
+  ],
+  runtimeCaching: [{
+    urlPattern: /api/,
+    handler: 'networkFirst'
+  }]
+};
