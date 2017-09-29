5cb2abc4ed76fcb2924ae6950bccbc4558016d0b
Weijia Wang <381152119@qq.com>
2017-09-29 10:45:43 +0800

    eslint

--- runtime.js
+++ runtime.js
@@ -1,4 +1,4 @@
-var defaults = require('lodash.defaults');
+var defaults = require('lodash.defaults')
 function hasSW() {
   return 'serviceWorker' in navigator &&
     // This is how I block Chrome 40 and detect Chrome 41, because first has
@@ -8,103 +8,103 @@ function hasSW() {
 }
 
 function install(url, options) {
-  options || (options = {});
-  options = defaults(options || {}, {});
+  options || (options = {})
+  options = defaults(options || {}, {})
 
   if (hasSW()) {
     if (!url) {
       console.warn('url is not defined, using \'/sw.js\' for defaults')
     }
     var registration = navigator.serviceWorker
-      .register(url || '/sw.js');
+      .register(url || '/sw.js')
 
 
     var handleUpdating = function (registration) {
-      var sw = registration.installing || registration.waiting;
-      var ignoreInstalling;
-      var ignoreWaiting;
+      var sw = registration.installing || registration.waiting
+      var ignoreInstalling
+      var ignoreWaiting
 
       // No SW or already handled
-      if (!sw || sw.onstatechange) return;
+      if (!sw || sw.onstatechange) return
 
-      var stateChangeHandler;
+      var stateChangeHandler
 
       // Already has SW
       if (registration.active) {
-        onUpdateStateChange();
-        stateChangeHandler = onUpdateStateChange;
+        onUpdateStateChange()
+        stateChangeHandler = onUpdateStateChange
       } else {
-        onInstallStateChange();
-        stateChangeHandler = onInstallStateChange;
+        onInstallStateChange()
+        stateChangeHandler = onInstallStateChange
       }
 
-      ignoreInstalling = true;
+      ignoreInstalling = true
       if (registration.waiting) {
-        ignoreWaiting = true;
+        ignoreWaiting = true
       }
 
-      sw.onstatechange = stateChangeHandler;
+      sw.onstatechange = stateChangeHandler
 
       function onUpdateStateChange() {
         switch (sw.state) {
-          case 'redundant': {
-            sendEvent('onUpdateFailed');
-            sw.onstatechange = null;
-          } break;
+        case 'redundant': {
+          sendEvent('onUpdateFailed')
+          sw.onstatechange = null
+        } break
 
-          case 'installing': {
-            if (!ignoreInstalling) {
-              sendEvent('onUpdating');
-            }
-          } break;
+        case 'installing': {
+          if (!ignoreInstalling) {
+            sendEvent('onUpdating')
+          }
+        } break
 
-          case 'installed': {
-            if (!ignoreWaiting) {
-              sendEvent('onUpdateReady');
-            }
-          } break;
+        case 'installed': {
+          if (!ignoreWaiting) {
+            sendEvent('onUpdateReady')
+          }
+        } break
 
-          case 'activated': {
-            sendEvent('onUpdated');
-            sw.onstatechange = null;
-          } break;
+        case 'activated': {
+          sendEvent('onUpdated')
+          sw.onstatechange = null
+        } break
         }
       }
 
       function onInstallStateChange() {
         switch (sw.state) {
-          case 'redundant': {
-            // Failed to install, ignore
-            sw.onstatechange = null;
-          } break;
+        case 'redundant': {
+          // Failed to install, ignore
+          sw.onstatechange = null
+        } break
 
-          case 'installing': {
-            // Installing, ignore
-          } break;
+        case 'installing': {
+          // Installing, ignore
+        } break
 
-          case 'installed': {
-            // Installed, wait activation
-          } break;
+        case 'installed': {
+          // Installed, wait activation
+        } break
 
-          case 'activated': {
-            sendEvent('onInstalled');
-            sw.onstatechange = null;
-          } break;
+        case 'activated': {
+          sendEvent('onInstalled')
+          sw.onstatechange = null
+        } break
         }
       }
-    };
+    }
 
     var sendEvent = function (event) {
       if (typeof options[event] === 'function') {
         options[event]({
           source: 'ServiceWorker'
-        });
+        })
       }
-    };
+    }
 
     registration.then(function (reg) {
       // WTF no reg?
-      if (!reg) return;
+      if (!reg) return
 
       // Installed but Shift-Reloaded (page is not controller by SW),
       // update might be ready at this point (more than one tab opened).
@@ -113,18 +113,18 @@ function install(url, options) {
       // to be controlled by SW. Maybe set flag to not reload it?
       // if (!navigator.serviceWorker.controller) return;
 
-      handleUpdating(reg);
+      handleUpdating(reg)
       if (options.autoUpdate) {
-        setInterval(update, 1000);
+        setInterval(update, 1000)
       }
       reg.onupdatefound = function () {
-        handleUpdating(reg);
-      };
+        handleUpdating(reg)
+      }
     }).catch(function (err) {
-      sendEvent('onError');
-      return Promise.reject(err);
-    });
-    return;
+      sendEvent('onError')
+      return Promise.reject(err)
+    })
+    return
   }
 }
 
@@ -132,29 +132,29 @@ function applyUpdate(callback, errback) {
   if (hasSW()) {
     navigator.serviceWorker.getRegistration().then(function (registration) {
       if (!registration || !registration.waiting) {
-        errback && errback();
-        return;
+        errback && errback()
+        return
       }
 
       registration.waiting.postMessage({
         action: 'skipWaiting'
-      });
+      })
 
-      callback && callback();
-    });
+      callback && callback()
+    })
 
-    return;
+    return
   }
 }
 
 function update() {
   if (hasSW()) {
     navigator.serviceWorker.getRegistration().then(function (registration) {
-      if (!registration) return;
-      return registration.update();
-    });
+      if (!registration) return
+      return registration.update()
+    })
   }
 }
-exports.install = install;
-exports.applyUpdate = applyUpdate;
-exports.update = update;
+exports.install = install
+exports.applyUpdate = applyUpdate
+exports.update = update
